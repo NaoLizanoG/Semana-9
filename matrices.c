0b09b1758d6e008e87a6e7189f7946e3d02d3f66
NaoLizanoG <naomilizano@gmail.com>
2023-05-23 12:37:40 -0500

    inicio del programa

---
+++ matrices.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+
+int main (){
+    //Se definen variables
+int filas, columnas;
+
+//Se pide que el usuario ingrese el tamaño de la matriz
+printf("Ingrese el numero de filas:");
+scanf("%d", &filas);
+printf("Ingrese el numero de columnas:");
+scanf("%d", &columnas);
+printf ("\n");
+int A[2][filas][columnas];
+
+
+return 0;
+}
