5d206c622834dc447fcd4c88cb190efeeb1711a0
NaoLizanoG <naomilizano@gmail.com>
2023-05-23 12:48:39 -0500

    Planteamiento inicia de la solucion que permite  impresion de la matriz original

--- matrices.c
+++ matrices.c
@@ -1,8 +1,12 @@
 # include<stdio.h>
+# include <time.h>
+# include <stdlib.h>
 
 int main (){
     //Se definen variables
 int filas, columnas;
+
+srand(time(NULL));
 
 //Se pide que el usuario ingrese el tamaño de la matriz
 printf("Ingrese el numero de filas:");
@@ -11,6 +15,27 @@ printf("Ingrese el numero de columnas:");
 scanf("%d", &columnas);
 printf ("\n");
 int A[2][filas][columnas];
+
+
+for (int j = 0; j < filas; j++)
+    {
+        for (int k = 0; k < columnas; k++)
+        {
+
+            //If para formar la matriz
+            if (k+1==columnas)
+            {
+             A[1][j][k]= rand()%101;
+            printf(" %d\n", A[1][j][k]);
+            }
+            else{
+                A[1][j][k]= rand()%101;
+            printf(" %d", A[1][j][k]);
+            
+            }
+        }
+    }
+
 
 
 return 0;
