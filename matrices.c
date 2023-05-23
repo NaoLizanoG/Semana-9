bf74d9c93adfead9078dcff11493b0ca13d838b6
NaoLizanoG <naomilizano@gmail.com>
2023-05-23 13:08:38 -0500

    Version final del codigo

--- matrices.c
+++ matrices.c
@@ -3,9 +3,11 @@
 # include <stdlib.h>
 
 int main (){
-    //Se definen variables
+
+//Se definen variables
 int filas, columnas;
 
+//Comando para los numeros aleatorios
 srand(time(NULL));
 
 //Se pide que el usuario ingrese el tamaño de la matriz
@@ -18,6 +20,8 @@ int A[2][filas][columnas];
 
 
 printf("La matriz original es:\n");
+
+//For para inicializar la matriz original
 for (int j = 0; j < filas; j++)
     {
         for (int k = 0; k < columnas; k++)
@@ -26,6 +30,7 @@ for (int j = 0; j < filas; j++)
             //If para formar la matriz
             if (k+1==columnas)
             {
+            //Inicializacion de la matriz con numeros aleatorios
              A[1][j][k]= rand()%101;
             printf("  %d\n", A[1][j][k]);
             }
@@ -40,6 +45,7 @@ for (int j = 0; j < filas; j++)
     printf ("\n");
     printf("La matriz transpuesta es:\n");
 
+    //For para inicalizar la matriz transpuesta
     for (int j = 0; j < columnas; j++)
     {
         for (int k = 0; k < filas; k++)
@@ -47,6 +53,7 @@ for (int j = 0; j < filas; j++)
             //If para formar la matriz
             if (k+1==filas)
             {
+            //Se intercambian  el numero de filas con el numero de columnas de la matriz original y viceversa
              A[2][j][k]= A[1][k][j];
             printf("  %d\n", A[2][j][k]);
             }
